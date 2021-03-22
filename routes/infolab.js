0a72b35ce9600becb13f47f80f90089fd98c91e9
khyoo1221 <khyoo1221@gmail.com>
2021-03-22 14:59:47 +0900

    first commit

---
+++ routes/infolab.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET page. */
+router.get('/', function(req, res, next) {
+  //console.log("get");  
+
+  res.render('infolab', { });
+});
+
+module.exports = router;
